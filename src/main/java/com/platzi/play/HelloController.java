985952e78a33cdb59bc1d2bb6285e60f3ef8e182
juandsw <juandavidgallegohernandez@gmail.com>
2025-09-23 13:50:50 -0500

    Commit Inicial

---
+++ src/main/java/com/platzi/play/HelloController.java
@@ -0,0 +1,22 @@
+package com.platzi.play;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private final String platform;
+    private final PlatziPlayAiService aiService;
+
+    public HelloController(@Value("${spring.application.name}")String platform, PlatziPlayAiService aiService) {
+        this.platform = platform;
+        this.aiService = aiService;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return this.aiService.generateGreeting(platform);
+    }
+}
---
+++ src/main/java/com/platzi/play/PlatziPlayAiService.java
@@ -0,0 +1,15 @@
+package com.platzi.play;
+
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
+import dev.langchain4j.service.spring.AiService;
+
+@AiService
+public interface PlatziPlayAiService {
+
+    @UserMessage("""
+            Genera un saludo de bienvenida a la plataforma de gestión de películas {{platform}}.   " +
+            Usa menos de 120 carácteres y hazlo con el estilo platzi.
+            """)
+    String generateGreeting(@V("platform") String platform);
+}
